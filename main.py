4b5fda0aca6b0154626fd55c9b4431e79e5b7f21
Dan Peretz <zikada@users.noreply.github.com>
2015-06-28 20:59:44 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import sys
+
 import pychess
 
 print("Welcome to PyChess, a CLI-based chess game using the Stockfish chess engine.")
@@ -8,13 +10,14 @@ board_state = [["R", "K", "T", "Q", "M", "T", "K", "R"], [" ", " ", " ", " ", " 
                [" ", " ", " ", " ", " ", " ", " ", " "], [" ", " ", " ", " ", " ", " ", " ", " "],
                [" ", " ", " ", " ", " ", " ", " ", " "], [" ", " ", " ", " ", " ", " ", " ", " "],
                [" ", " ", " ", " ", " ", " ", " ", " "], ["R", "K", "T", "Q", "M", "T", "K", "R"]]
-while True:
-    try:
-        print("\n" + pychess.parse_board(board_state))
-        action = str(input("Your move: "))
-        pychess.action_validate(action)
-    except InvalidAction:
-        print("Invalid action.")
-    else:
-        print("Thinking...")
-        board_state = pychess.computer_move(board_state)
+try:
+    print("\n" + pychess.parse_board(board_state))
+    action = str(input("Your move (type \"quit\" or \"exit\" to quit): "))
+    if ("quit" or "exit") in action:
+        sys.exit(0)
+    pychess.action_validate(action)
+except:
+    print("Invalid action.")
+else:
+    print("Thinking...")
+    board_state = pychess.computer_move(board_state)
